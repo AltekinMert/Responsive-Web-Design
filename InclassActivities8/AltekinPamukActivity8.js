12ea7f16007970341fd1bb89e4fc0fc23aad44bb
Mert Altekin <altekinmert.ceng@gmail.com>
2024-05-03 15:37:29 +0300

    Inclassactivities8

---
+++ InclassActivities8/AltekinPamukActivity8.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+            $("#birthday").datepicker();
+        });
+
+
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $("#language").autocomplete({
+      source: availableTags
+    });
+  } );
